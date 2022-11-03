22f633a02d8e585df20b92c3464eda0bbaadcf55
adoniran <adoniranfranca@gmail.com>
2022-11-03 19:03:40 -0300

    Primeira atualização do sistema bancário

--- index.rb
+++ index.rb
@@ -1,1 +1,36 @@
 puts "Seja Bem Vindo ao nosso banco"
+puts "_____________________________"
+puts ""
+
+puts "Digite seu primeiro nome"
+nome = gets.chomp
+puts "----------------"
+
+class Conta
+	attr_reader :usuario, :numero_da_conta, :saldo
+
+	def initialize(nome, numero, valor)
+		@usuario = nome
+		@numero_da_conta = numero
+		@saldo = valor
+	end
+
+
+	def informacoes
+		puts "Olá #{@usuario}"
+		puts ""
+		puts "Algumas informações sobre sua conta:"
+		puts "-------------------"
+		puts "Numero da conta é:"
+		puts "#{@numero_da_conta}"
+		puts "-------------------"
+		puts "Saldo disponível é: #{@saldo}" 
+	end
+end
+
+
+
+
+conta = Conta.new(nome, "0001", 0.0)
+
+conta.informacoes
