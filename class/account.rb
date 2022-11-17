2edf11e85374ebe13634de7b2687e2f19876c79f
adoniran <adoniranfranca@gmail.com>
2022-11-17 16:47:07 -0300

    Opção adicionada

--- class/account.rb
+++ class/account.rb
@@ -1,20 +1,30 @@
 class Account
-	attr_reader :user, :account_number, :balance
+	attr_reader :name, :balance, :number, :last_value_add
 
-	def initialize(user, account_number, balance)
-		@user = user
-		@account_number = account_number
+
+	def initialize(name, number, balance)
+		@name = name
+		@number = number
 		@balance = balance
+		
 	end
+	
+	def sum(value)
+		@last_value_add = value
+
+		@balance = @balance + @last_value_add
+	end
+
   
 	def information
 		puts "-------------------"
-		puts "Nome: #{@user}"
+		puts "Nome: #{@name}"
 		puts "-------------------"
 		puts "Numero da conta:"
-		puts "#{@account_number}"
+		puts "#{@number}"
 		puts "-------------------"
 		puts "Saldo disponível: #{@balance}" 
+		
 	end
 end
 
